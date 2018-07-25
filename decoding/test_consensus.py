26687989c138a60ebe9889460880ab92fb4d1cab
jordisr <jordisr@users.noreply.github.com>
2018-07-24 22:47:48 -0700

    added unit test

--- decoding/test_consensus.py
+++ decoding/test_consensus.py
@@ -22,7 +22,7 @@ import numpy as np
 from collections import OrderedDict
 
 from testing import profile, joint_profile
-from consensus import pair_label_prob, pair_prefix_search_vec, pair_gamma, pair_forward
+from consensus import pair_label_prob, pair_prefix_search_vec, pair_gamma, pair_forward, prefix_search_vec
 
 class TestDecoding(unittest.TestCase):
 
@@ -96,6 +96,27 @@ class TestVectorizedDecoding(unittest.TestCase):
 
     def test_something_else(self):
         self.assertTrue(1)
+
+class TestVectorized1D(unittest.TestCase):
+
+    def test_prefix_search_vec(self):
+
+        def helper(y):
+            alphabet = ('A','B','')
+            toy_alphabet = OrderedDict([('A',0),('B',1)])
+            prof = profile(y,alphabet)
+            top_label = prof.top_label()
+            search_top_label = prefix_search_vec(y,alphabet=toy_alphabet)
+            return((top_label[0] == search_top_label[0]) and np.isclose(top_label[1], search_top_label[1]))
+
+        y = np.array([[0.1,0.6,0.3],[0.4,0.2,0.4],[0.4,0.3,0.3],[0.2,0.8,0]])
+        self.assertTrue(helper(y))
+
+        y = np.array([[0.7,0.2,0.1],[0.2,0.3,0.5],[0.7,0.2,0.1],[0.05,0.05,0.9]])
+        self.assertTrue(helper(y))
+
+        y = np.array([[0.7,0.2,0.1],[0.2,0.3,0.5]])
+        self.assertTrue(helper(y))
 
 if __name__ == '__main__':
     unittest.main()
