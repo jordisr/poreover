b7b06b15c02d3fe7df7936ee9c25725b8f6a7419
jordisr <jordisr@users.noreply.github.com>
2018-09-27 18:17:01 -0700

    fixed cython compilation options for macOS

--- decoding/__init__.py
+++ decoding/__init__.py
@@ -1,4 +1,3 @@
 from .decoding import *
-#from . import decoding2
 from . import decoding_cpp
-from . import sparse_envelope
+from . import decoding_cy
--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ from distutils.extension import Extension
 import numpy as np
 
 ext_modules = [
-Extension("decoding.decoding_cpp", sources=["decoding/decoding_cpp.pyx"], include_dirs=[np.get_include()], language='c++'),
-Extension("decoding.decoding_cy", sources=["decoding/decoding_cy.pyx"], include_dirs=[np.get_include()], language='c++'),
-#Extension("decoding.decoding2", sources=["decoding/decoding2.pyx"], include_dirs=[np.get_include()]),
-#Extension("decoding.sparse", sources=["decoding/sparse.pyx"], include_dirs=[np.get_include()]),
+Extension("decoding.decoding_cpp", sources=["decoding/decoding_cpp.pyx"], include_dirs=[np.get_include()], language='c++',extra_link_args=['-std=c++11','-stdlib=libc++'], extra_compile_args=['-std=c++11','-stdlib=libc++']),
+Extension("decoding.decoding_cy", sources=["decoding/decoding_cy.pyx"], include_dirs=[np.get_include()], language='c++',extra_link_args=['-std=c++11','-stdlib=libc++'], extra_compile_args=['-std=c++11','-stdlib=libc++']),
 Extension("align.align", sources=["align/align.pyx"], include_dirs=[np.get_include()])
 ]
 
