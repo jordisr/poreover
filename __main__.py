b8ef0a320b2e2be7b6c66a5ed6267f5ae6a266e1
jordisr <jordisr@users.noreply.github.com>
2019-09-09 02:05:55 -0700

    improved training, calling multiple FAST5 only loads model once

--- __main__.py
+++ __main__.py
@@ -33,13 +33,10 @@ parser_call = subparsers.add_parser('call', help='Base call one or multiple read
 parser_call.set_defaults(func=call)
 parser_call.add_argument('--model', default=os.path.join(script_dir,'models/r9.5'), help='Trained model to load (if directory, loads latest from checkpoint file)')
 parser_call.add_argument('--scaling', default='standard', choices=['standard', 'current', 'median', 'rescale'], help='Type of preprocessing (should be same as training)')
-parser_call.add_argument('--signal', help='File with space-delimited signal for testing')
-parser_call.add_argument('--fast5', default=False, help='Single FAST5 file or directory of FAST5 files')
+parser_call.add_argument('--fast5', default=False, help='Single FAST5 file or directory of FAST5 files', required=True)
 parser_call.add_argument('--out', default='out', help='Prefix for sequence output')
 parser_call.add_argument('--window', type=int, default=400, help='Call read using chunks of this size')
 parser_call.add_argument('--format', choices=['csv', 'npy'], default='npy', help='Save softmax probabilities to CSV file or logits to binarized NumPy format')
-parser_call.add_argument('--decoding', default='greedy', choices=['greedy','beam', 'prefix', 'none'], help='Choice of CTC decoding algorithm to use. Greedy takes best path. Beam uses TensorFlow\'s built-in beam search. Prefix uses CTC prefix search decoding (but does not collapse repeated characters). None skips decoding and just runs neural network (output can be saved with --logits)')
-parser_call.add_argument('--threads', type=int, default=1, help='Number of threads to use for prefix decoding')
 parser_call.add_argument('--no_stack', default=False, action='store_true', help='Basecall [1xSIGNAL_LENGTH] tensor instead of splitting it into windows (slower)')
 
 # Decode
--- network/network.py
+++ network/network.py
@@ -1,9 +1,11 @@
 import numpy as np
 import tensorflow as tf
 import sys, os
+import h5py
+import glob
 from pathlib import Path
 
-BATCH_SIZE = 64
+INPUT_DIM=1
 
 def rnn(num_neurons=128, num_labels=4, input_size=1000):
     return tf.keras.Sequential([tf.keras.layers.Bidirectional(tf.keras.layers.GRU(num_neurons, return_sequences=True), input_shape=(input_size,1)),
@@ -18,16 +20,15 @@ def cnn_rnn(num_neurons=128, num_labels=4, input_size=1000):
     tf.keras.layers.Bidirectional(tf.keras.layers.GRU(num_neurons, return_sequences=True)),
     tf.keras.layers.Dense(num_labels+1, activation=None)])
 
-def train_ctc_model(model, dataset, epochs=1, optimizer=tf.keras.optimizers.Adam(), save_frequency=10, log_frequency=10, log_file=sys.stderr, ctc_merge_repeated=False):
+def train_ctc_model(model, dataset, optimizer=tf.keras.optimizers.Adam(), save_frequency=10, log_frequency=10, log_file=sys.stderr, ctc_merge_repeated=False):
     avg_loss = []
     checkpoint = 0
     checkpoint_dir = 'saved'
     t=0
-    for X,y in dataset.shuffle(buffer_size=1000).repeat(epochs):
-        sequence_length = tf.ones(BATCH_SIZE, dtype=np.int32)*y.shape[1]
+    for X,y in dataset:
+        sequence_length = tf.ones(X.shape[0], dtype=np.int32)*1000
         with tf.GradientTape() as tape:
             y_pred = model(X)
-            #print(X.shape, y.shape, y_pred.shape)
             loss = tf.reduce_mean(tf.compat.v1.nn.ctc_loss(inputs=y_pred,
                                             labels=y,
                                             sequence_length=sequence_length,
@@ -76,86 +77,9 @@ def train(args):
             model_file = args.restart
         model.load_weights(model_file)
 
-    avg_loss = train_ctc_model(model, dataset.batch(BATCH_SIZE, drop_remainder=True), epochs=args.epochs, save_frequency=args.save_every, log_frequency=args.loss_every, log_file=log_file, ctc_merge_repeated=args.ctc_merge_repeated)
-    print(avg_loss, sys.stderr)
+    train_ctc_model(model, dataset.shuffle(buffer_size=5000).repeat(args.epochs).batch(64, drop_remainder=True), save_frequency=args.save_every, log_frequency=args.loss_every, log_file=log_file)
 
 def call(args):
-    WINDOW_SIZE = args.window
-
-    if args.fast5:
-        if os.path.isdir(args.fast5):
-            fast5_files =  glob.glob(args.fast5+'/*.fast5')
-            print("Found",len(fast5_files),"files to basecall")
-            for fast5 in fast5_files:
-                args.fast5 = fast5
-                if args.logits:
-                    # use different filename for each saved logit
-                    args.out = os.path.basename(fast5)
-                call(args)
-            sys.exit()
-        else:
-            hdf = h5py.File(args.fast5,'r')
-
-            # basic parameters
-            read_string = list(hdf['/Raw/Reads'].keys())[0]
-            read_id = hdf['/Raw/Reads/'+read_string].attrs['read_id']
-            read_start_time = hdf['/Raw/Reads/'+read_string].attrs['start_time']
-            read_duration = hdf['/Raw/Reads/'+read_string].attrs['duration']
-
-            # raw events and signals
-            raw_signal_path = '/Raw/Reads/'+read_string+'/Signal'
-            raw_signal = hdf[raw_signal_path]
-            assert(len(raw_signal) == read_duration)
-
-            # for converting raw signal to current (pA)
-            alpha = hdf['UniqueGlobalKey']['channel_id'].attrs['digitisation'] / hdf['UniqueGlobalKey']['channel_id'].attrs['range']
-            offset = hdf['UniqueGlobalKey']['channel_id'].attrs['offset']
-            sampling_rate = hdf['UniqueGlobalKey']['channel_id'].attrs['sampling_rate']
-
-            raw_signal = [s for s in raw_signal if 200 < s < 800] # very rough heuristic for abasic region
-
-            # rescale signal (should be same as option selected in make_labeled_data.py)
-            if args.scaling == 'standard':
-                # standardize
-                signal = (raw_signal - np.mean(raw_signal))/np.std(raw_signal)
-            elif args.scaling == 'current':
-                # convert to current
-                signal = (raw_signal+offset)/alpha # convert to pA
-            elif args.scaling == 'median':
-                # divide by median
-                signal = raw_signal / np.median(raw_signal)
-            elif args.scaling == 'rescale':
-                signal = (raw_signal - np.mean(raw_signal))/(np.max(raw_signal) - np.min(raw_signal))
-
-    elif args.signal:
-        raw_events = []
-        with open(args.signal, 'r') as f:
-            for line in f:
-                if len(line.split()) > 1:
-                    raw_events.append(np.array(list(map(lambda x: float(x),line.split()))))
-        # pad input sequences
-        #sizes = [len(i) for i in raw_events]
-        #padded_X = np.reshape(batch.pad(raw_events), (len(raw_events), -1, INPUT_DIM))
-        signal = raw_events[0] # just take first line, don't normalize
-
-    else:
-        sys.exit("An input file must be specified with --signal or --fast5!")
-
-    if not args.no_stack:
-        # split signal into blocks to allow for faster basecalling with the GPU
-        rounded = int(len(signal)/WINDOW_SIZE)*WINDOW_SIZE
-        stacked = np.reshape(signal[:rounded], (-1, WINDOW_SIZE, INPUT_DIM))
-        sizes = [len(i) for i in stacked]
-
-        if rounded < len(signal):
-            last_row = np.zeros(WINDOW_SIZE)
-            last_row[:len(signal)-rounded] = signal[rounded:]
-            last_row = np.expand_dims(last_row,1)
-            sizes.append(len(signal)-rounded)
-            stacked = np.vstack((stacked,np.expand_dims(last_row,0)))
-    else:
-        stacked = np.reshape(np.expand_dims(signal,axis=1), (1, len(signal), INPUT_DIM))
-        sizes = [len(i) for i in stacked]
 
     # if model argument is a directory load the latest model in it
     if os.path.isdir(args.model):
@@ -166,12 +90,78 @@ def call(args):
     model = rnn()
     model.load_weights(model_file)
 
-    logits_ = model.call(stacked)
+    if args.fast5:
+        if os.path.isdir(args.fast5):
+            fast5_files =  glob.glob(args.fast5+'/*.fast5')
+            print("Found",len(fast5_files),"files to basecall")
+            for fast5 in fast5_files:
+                args.fast5 = fast5
+                args.out = os.path.basename(fast5)
+                call_helper(args, model)
+        else:
+            call_helper(args, model)
+    else:
+        sys.exit("An input file must be specified with --fast5!")
+
+def call_helper(args, model):
+
+    # read raw signal from FAST5 file
+    hdf = h5py.File(args.fast5,'r')
+
+    # basic parameters
+    read_string = list(hdf['/Raw/Reads'].keys())[0]
+    read_id = hdf['/Raw/Reads/'+read_string].attrs['read_id']
+    read_start_time = hdf['/Raw/Reads/'+read_string].attrs['start_time']
+    read_duration = hdf['/Raw/Reads/'+read_string].attrs['duration']
+
+    # raw events and signals
+    raw_signal_path = '/Raw/Reads/'+read_string+'/Signal'
+    raw_signal = hdf[raw_signal_path]
+    assert(len(raw_signal) == read_duration)
+
+    # for converting raw signal to current (pA)
+    alpha = hdf['UniqueGlobalKey']['channel_id'].attrs['digitisation'] / hdf['UniqueGlobalKey']['channel_id'].attrs['range']
+    offset = hdf['UniqueGlobalKey']['channel_id'].attrs['offset']
+    sampling_rate = hdf['UniqueGlobalKey']['channel_id'].attrs['sampling_rate']
+
+    raw_signal = [s for s in raw_signal if 200 < s < 800] # very rough heuristic for abasic region
+
+    # rescale signal (should be same as option selected in make_labeled_data.py)
+    if args.scaling == 'standard':
+        # standardize
+        signal = (raw_signal - np.mean(raw_signal))/np.std(raw_signal)
+    elif args.scaling == 'current':
+        # convert to current
+        signal = (raw_signal+offset)/alpha # convert to pA
+    elif args.scaling == 'median':
+        # divide by median
+        signal = raw_signal / np.median(raw_signal)
+    elif args.scaling == 'rescale':
+        signal = (raw_signal - np.mean(raw_signal))/(np.max(raw_signal) - np.min(raw_signal))
+
+    if not args.no_stack:
+        # split signal into blocks to allow for faster basecalling with the GPU
+        rounded = int(len(signal)/args.window)*args.window
+        stacked = np.reshape(signal[:rounded], (-1, args.window, INPUT_DIM))
+        sizes = [len(i) for i in stacked]
+
+        if rounded < len(signal):
+            last_row = np.zeros(args.window)
+            last_row[:len(signal)-rounded] = signal[rounded:]
+            last_row = np.expand_dims(last_row,1)
+            sizes.append(len(signal)-rounded)
+            stacked = np.vstack((stacked,np.expand_dims(last_row,0)))
+    else:
+        stacked = np.reshape(np.expand_dims(signal,axis=1), (1, len(signal), INPUT_DIM))
+        sizes = [len(i) for i in stacked]
+
+    # run forward pass
+    logits_ = model(stacked)
     softmax = tf.nn.softmax(logits_)
 
     # now just saving logits, can use decode submodule for decoding probabilities
     logits_concatenate = np.concatenate(softmax)[:-1*max(sizes[0]-sizes[-1], 1)]
-    if args.logits == 'csv':
+    if args.format == 'csv':
         np.savetxt(args.out+'.csv', logits_concatenate, delimiter=',', header=','.join(['A','C','G','T','']), comments='', )
     else:
         np.save(args.out, logits_concatenate)
