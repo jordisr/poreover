d778a33754d29a9d3369deeb33e3c8d44c1d9eb5
jordisr <jordisr@users.noreply.github.com>
2020-01-27 17:25:26 -0800

    SparseMatrix correctly handles non-numeric default values

--- decoding/SparseMatrix.h
+++ decoding/SparseMatrix.h
@@ -10,14 +10,17 @@ class SparseRow {
   public:
     int start, end;
     T* values;
+    T default_value;
 
-    SparseRow(int s, int e) {
+    SparseRow(int s, int e, T d) {
       start = s;
       end = e;
       values = new T[e-s+1];
       start = s;
       end = e;
+      default_value = d;
     };
+
     ~SparseRow() {
         delete [] values;
     }
@@ -47,7 +50,7 @@ class SparseRow {
 
     const T get(int i) {
       if ((i < start) || (i > end)) {
-        return -std::numeric_limits<T>::infinity();
+        return default_value;
       } else {
         return values[i-start];
       }
@@ -62,8 +65,9 @@ class SparseMatrix {
     int length;
     T default_value;
 
-    SparseMatrix() :length{0}, default_value{-std::numeric_limits<T>::infinity()} {}
     SparseMatrix(T d) :length{0}, default_value{d} {}
+    // if no default value provided, try to use -infinity
+    SparseMatrix() :length{0}, default_value{-std::numeric_limits<T>::infinity()} {}
 
     ~SparseMatrix() {
         for (auto x : row) {
@@ -83,14 +87,18 @@ class SparseMatrix {
       }
     }
 
-    void push_row(int start, int end) {
-      //std::cout << "Pushing row from " << start << " to " << end << std::endl;
-      SparseRow<T>* rowPtr = new SparseRow<T>(start, end);
+    void push_row(int start, int end, T value) {
+      // set row to some value
+      SparseRow<T>* rowPtr = new SparseRow<T>(start, end, default_value);
       row.push_back(rowPtr);
       for (int v=start; v<=end; v++) {
-        rowPtr->set(v, default_value);
+        rowPtr->set(v, value);
       }
       length += 1;
+    }
+
+    void push_row(int start, int end) {
+      push_row(start, end, default_value);
     }
 
     void set(int i, int j, T value) {
@@ -99,7 +107,7 @@ class SparseMatrix {
       }
     }
 
-    T get(int i, int j) {
+    const T get(int i, int j) {
       if ((0 <= i) && (i < length)) {
         return row[i]->get(j);
       } else {
